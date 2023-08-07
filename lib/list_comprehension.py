2ad3474d3da2121726a9fb3478ba11b053afce45
Michael Lombard <miklombard@yahoo.com>
2023-08-06 21:30:33 -0400

    completed lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,25 @@
 #!/usr/bin/env python3
 
+import ipdb
+
 def return_evens(num_list):
-    pass
+    even_numbers = []
+    for num in num_list:
+        if num % 2 == 0:
+            even_numbers.append(num)
+    if even_numbers:
+            return even_numbers
+    else:
+            return[]
+    
+
+
+    
 
 def make_exclamation(sentence_list):
-    pass
+    add_exclamation = []
+    for sentence in sentence_list:
+         exc_point = sentence + "!"
+         add_exclamation.append(exc_point)
+    return add_exclamation
+#ipdb.set_trace()
